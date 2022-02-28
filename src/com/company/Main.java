3803c5c294496b643b39964294210ef32e198d92
mads94i5 <mads94i5@stud.kea.dk>
2022-02-28 17:25:34 +0100

    Working testing condition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,31 @@ package com.company;
 
 public class Main {
 
+    public char[] chars = { ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å' };
+
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+        main.runProgram();
+    }
+    public void runProgram()
+    {
+        System.out.println("'C' is converted to number: " + convertToNumber('C'));
+        System.out.println("'5' is converted to letter: " + convertToLetter(5));
+    }
+    public int convertToNumber(char ch)
+    {
+        int returnValue = -1;
+        for (int i = 0; i < chars.length; i++)
+        {
+            if (ch == chars[i])
+            {
+                returnValue = i;
+            }
+        }
+        return returnValue;
+    }
+    public char convertToLetter(int number)
+    {
+        return chars[number];
     }
 }
